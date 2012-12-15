692943e0c16081fb329923fde1c6c2831ea974da
Zze <jz3731@gmail.com>
2012-12-14 16:49:03 -1000

    Revert "Fix conflict / break code"
    This reverts commit 82846a6e654a7d8afb8fa75766a7c2c27c2a78ca, reversing
    changes made to 3278cb8040842282b33ef454cec4009ddeca3b88.

--- conf_multiRealm.php
+++ conf_multiRealm.php
@@ -1,25 +1,11 @@
 <?php
-/*
- * Copyright (C) syncApp Zze jz3731@gmail.com
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+/* Todo */
+//$SameLoginDetails = TRUE;
 
 // The ID must match the realmID for spesfic character database.
-// Example: $databases['1'] is realm_ID 1. 'character_db_name' => "characters" is the characters database name.
-// TODO: Unique database details
-
-    $databases['1'] = array('character_db_name' => "characters",          /* 'user' => "trinity", 'pass' => "trinity" */ );
-    // $databases['2'] = array('character_db_name' => "characters_db_2",   /* 'user' => "trinity", 'pass' => "trinity" */ );
-    // $databases['3'] = array('character_db_name' => "characters_db_3",   /* 'user' => "trinity", 'pass' => "trinity" */ );
+// $databases['1'] is equal to realm_ID 1
+//
+$databases['1'] = array('db' => "characters",       /* 'user' => "trinity", 'pass' => "trinity" */);
+$databases['2'] = array('db' => "characters_db2",   /* 'user' => "trinity", 'pass' => "trinity" */ );
+$databases['3'] = array('db' => "characters_db3",   /* 'user' => "trinity", 'pass' => "trinity" */ );
+?>
